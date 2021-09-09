c6f208deb987232efd76410ced5a22040426e622
samsuh <samsuh@gmail.com>
2021-09-10 08:24:40 +0900

    production config for package.json and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const path = require('path')
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: "bundle.js", 
-  }, 
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/, 
-      exclude: /node_modules/
-    },{
-      test: /\.?css$/,
-      use: [
-        'style-loader',
-        'css-loader', 
-        'sass-loader'
-      ]
-    }]
-  },
-  mode: "development",
-  devtool: 'eval-cheap-module-source-map', 
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-  }  
+module.exports = (env) => {
+  const isProduction = env === 'production'
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    mode: 'development',
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+    },
+  }
 }
